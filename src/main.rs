bbb56a849c332454e967e28ad2ec69b244a127ed
Dylan Conrad Johnson <djohnson@ancestry.com>
2015-09-13 16:17:23 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use libhackit_v2::parse::{Parser, ParserTrait};
 use libhackit_v2::eval::{Eval, EvalTrait, PrettyPrint};
 
 fn main() {
-    // let lexer = Lexer::new("((set a (params b) (println (add -3.1 4 2))) (a 3))");
-    // let lexer = Lexer::new("(println (add -2.1 (add 3 4) 2))(println 3)");
-    let lexer = Lexer::new("((set (name a) (params) (body (println (add 4 2)))) (println 333) (a))");
+    let lexer = Lexer::new("((set (name a) (params b c) (body (println (add 5 (add (c) (b)) 20))))(a 90 1))");
+    // let lexer = Lexer::new("((set (name a) (params) (body (println (add 3 (add 3 4) 20))))(a))");
     let mut parser = Parser::new();
     let mut eval = Eval::new_option(match lexer.lex() {
         Some(tokens) => {
